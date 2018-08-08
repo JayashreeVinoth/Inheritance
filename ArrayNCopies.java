1b27718cb374f40bb2a01ce5bc3aef589832db6e
JayashreeVinoth <39224240+JayashreeVinoth@users.noreply.github.com>
2018-08-08 08:26:52 +0000

    Add files via upload
    Arraylist - 4 types of initialization

---
+++ ArrayNCopies.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class ArrayNCopies {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> copy = new ArrayList<Integer>(Collections.nCopies(100,2));
+		System.out.println(copy);
+	}
+
+}
